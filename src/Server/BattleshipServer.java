2751d885ea0f0a3bff25af115ac93c1b662991c5
Marco Ocaña <64444600+Dios-Ocana@users.noreply.github.com>
2021-10-10 16:57:51 -0500

    Update BattleshipServer.java
    Server's shoot-logic is not longer random. Username now is referenced for each client's turn

--- src/Server/BattleshipServer.java
+++ src/Server/BattleshipServer.java
@@ -38,7 +38,8 @@ public class BattleshipServer {
         String shoot;
         StringBuilder sb = new StringBuilder();
         boolean flag = false;
-        int x, y;
+        boolean hit_target = false;
+        int x=0, y=0, op, cont=0;
         try {
             server = new DatagramSocket(Properties.PORT, InetAddress.getByName(Properties.SERVER_IP));
             server.setReuseAddress(true);
@@ -58,7 +59,7 @@ public class BattleshipServer {
                 packet = new DatagramPacket(new byte[65535], 65535);
                 server.receive(packet);
                 if (new String(packet.getData(), 0, packet.getLength()).equals("ready")) {
-                    System.out.println("User is ready");
+                    System.out.println(username + " is ready");
                     for (Ship ship : ships) {
                         oos.writeObject(ship);
                         oos.flush();
@@ -74,8 +75,101 @@ public class BattleshipServer {
                     prevShoots = new ArrayList<>();
                     clientAttempts = 0;
                     while(!end){
-                        System.out.println((serverTurn) ? "Server's turn" : "Client's turn");
+                        System.out.println((serverTurn) ? "Server's turn" : username+"'s turn");
                         if(serverTurn){
+                            if(hit_target)
+                            {
+                                do {
+                                    if(x>0 && y>0 && x<9 && y<9)
+                                    {
+                                    op = RANDOM.nextInt(4);
+                                    switch(op)
+                                    {
+                                        case 0: x=x+1; break;
+                                        case 1: y=y+1; break;
+                                        case 2: x=x-1; break;
+                                        case 3: y=y-1; break;
+                                    }
+                                    }
+                                else
+                                    if(x>0 && y>0 && x>=9 && y<9)
+                                    {
+                                    op = RANDOM.nextInt(3);
+                                    switch(op)
+                                    {
+                                        case 0: x=x-1; break;
+                                        case 1: y=y+1; break;
+                                        case 2: y=y-1; break;
+                                    }
+                                    }
+                                else
+                                    if(x>0 && y>0 && x<9 && y>=9)
+                                    {
+                                    op = RANDOM.nextInt(3);
+                                    switch(op)
+                                    {
+                                        case 0: x=x+1; break;
+                                        case 1: x=x-1; break;
+                                        case 2: y=y-1; break;
+                                    }
+                                    }
+                                else
+                                    if(x<=0 && y>0 && x<9 && y<9)
+                                    {
+                                    op = RANDOM.nextInt(3);
+                                    switch(op)
+                                    {
+                                        case 0: x=x+1; break;
+                                        case 1: y=y+1; break;
+                                        case 2: y=y-1; break;
+                                    }
+                                    }
+                                else
+                                    if(x>0 && y<=0 && x<9 && y<9)
+                                    {
+                                    op = RANDOM.nextInt(3);
+                                    switch(op)
+                                    {
+                                        case 0: y=y+1; break;
+                                        case 1: x=x+1; break;
+                                        case 2: x=x-1; break;
+                                    }
+                                    }
+                                else
+                                    if(x<=0 && y<=0)
+                                    {
+                                    op = RANDOM.nextInt(2);
+                                    switch(op)
+                                    {
+                                        case 0: y=y+1; break;
+                                        case 1: x=x+1; break;
+                                    }
+                                    }
+                                 else
+                                    if(x>=9 && y>=9)
+                                    {
+                                    op = RANDOM.nextInt(2);
+                                    switch(op)
+                                    {
+                                        case 0: y=y-1; break;
+                                        case 1: x=x-1; break;
+                                    }
+                                    }
+                                
+                                sb.append(x);
+                                sb.append(',');
+                                sb.append(y);
+                                shoot = sb.toString();
+                                sb.setLength(0);
+                                if(!prevShoots.contains(shoot)) {
+                                    prevShoots.add(shoot);
+                                    flag = false;
+                                } else {
+                                    flag = true;
+                                }
+                            } while(flag);
+                            }
+                            else{
                             do {
                                 x = RANDOM.nextInt(10);
                                 y = RANDOM.nextInt(10);
@@ -91,6 +185,7 @@ public class BattleshipServer {
                                     flag = true;
                                 }
                             } while(flag);
+                            }
                             buff = shoot.getBytes();
                             packet = new DatagramPacket(buff, buff.length);
                             server.send(packet);
@@ -104,8 +199,15 @@ public class BattleshipServer {
                             server.receive(packet);
                             end = Boolean.parseBoolean(new String(packet.getData(), 0, packet.getLength()));
                             System.out.println((end) ? "Game over, server wins": "Ships left: " + shipsLeft);
+                            if(serverTurn){
+                                hit_target = true;
+                            }
+                            else
+                            {
+                                hit_target = false;
+                            }
                         } else {
-                            clientTurn();
+                            clientTurn(username);
                         }
                     }
                 } else {
@@ -118,14 +220,14 @@ public class BattleshipServer {
         }
     }
     
-    private static void clientTurn() throws Exception{
+    private static void clientTurn(String username) throws Exception{
         String clientShoot;
         int x, y;
         buff = new byte[65535];
         packet = new DatagramPacket(buff, 65535);
         server.receive(packet);
         clientShoot = new String(packet.getData(), 0, packet.getLength());
-        System.out.println("Client's shoot " + clientShoot);
+        System.out.println(username + "'s shoot " + clientShoot);
         if (clientShoot.substring(1, 2).equals(",")) {
             x = Integer.parseInt(clientShoot.substring(0, 1));
             y = Integer.parseInt(clientShoot.substring(2, 3));
@@ -147,7 +249,7 @@ public class BattleshipServer {
             packet = new DatagramPacket(buff, buff.length);
             server.send(packet);
             if(shipsLeft == 0){
-                System.out.println("Game over, client wins");
+                System.out.println("Game over, " + username + " wins");
                 end = true;
             }
             if(clientAttempts == 3){
@@ -159,10 +261,11 @@ public class BattleshipServer {
                 }
             }
         } else {
-            throw new Exception("Client shoot failed");
+            throw new Exception( username + "shoot failed");
         }
     }
-
+    
+    
     private static void setShipsCoordenates(Ship[] ships) {
         int x, y;
         boolean flag = false, vertical = false;
